64615954510e602beb88f9c4be6c9a3bcf4bcad1
marcosgintonic <marcosgin291@gmail.com>
2019-07-17 10:31:52 -0300

    Using Link to page home in Logo component

--- templates/shared/Header/Logo/index.jsx
+++ templates/shared/Header/Logo/index.jsx
@@ -1,21 +1,27 @@
 import React from "react";
+import Link from "next/link";
 
 function Logo(props) {
   const { url, svg } = props;
-  if (url) {
-    return (
-      <a className="telefe-logo" style={{ backgroundImage: `url(${url})` }}>
-        <h2>Telefe</h2>
-        {svg && svg}
-      </a>
-    );
-  } else {
-    return (
-      <a className="telefe-logo default-logo">
-        <h2>Telefe</h2>
-      </a>
-    );
-  }
+
+  const renderLogo = () => {
+    if (url) {
+      return (
+        <a className="telefe-logo" style={{ backgroundImage: `url(${url})` }}>
+          <h2>Telefe</h2>
+          {svg && svg}
+        </a>
+      );
+    } else {
+      return (
+        <a className="telefe-logo default-logo">
+          <h2>Telefe</h2>
+        </a>
+      );
+    }
+  };
+
+  return <Link href="/">{renderLogo()}</Link>;
 }
 
 export default Logo;
