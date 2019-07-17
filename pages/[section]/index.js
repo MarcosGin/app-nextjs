c1ebacfb145f5cd7a6a7a86a7c703a4af08ec7cb
marcosgintonic <marcosgin291@gmail.com>
2019-07-17 15:02:58 -0300

    Get data fron api endpoint and render the cards

--- pages/[section]/index.js
+++ pages/[section]/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { withRouter } from "next/router";
+import axios from "axios";
+
 import BaseLayout from "../../templates/layouts/BaseLayout";
+import SectionPosts from "../../components/SectionPosts";
 
 class Section extends React.Component {
-  static async getInitialProps() {
-    return {};
+  static async getInitialProps({ query }) {
+    const { section } = query;
+    const sectionData = await axios.get(`http://localhost:3000/api/${section}`);
+    return {
+      sectionData: sectionData.data
+    };
   }
 
   constructor(props) {
@@ -13,10 +21,12 @@ class Section extends React.Component {
   render() {
     return (
       <BaseLayout {...this.props}>
-        <h1>Section index</h1>
+        <div className="section ">
+          <SectionPosts {...this.props.sectionData} />
+        </div>
       </BaseLayout>
     );
   }
 }
 
-export default Section;
+export default withRouter(Section);
