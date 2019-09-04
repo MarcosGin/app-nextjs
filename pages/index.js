e1cdef5342df23c3604d96b4ab198c7b504b98c0
Marcos Gin <marcosgin291@gmail.com>
2019-09-04 14:04:22 -0300

    Load Home page from json

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,17 @@
 import React from "react";
 import axios from "axios";
-import BaseLayout from "../templates/layouts/BaseLayout";
-import SectionCard from "../components/SectionCard";
-import Player from "../components/Player";
+import BasePage from "../templates/layouts/BasePage";
 
 class Index extends React.Component {
   static async getInitialProps() {
-    const { data: sectionsData } = await axios.get("http://localhost:3000/api/sections/home");
+    const { data: page } = await axios.get("http://localhost:3000/api/pages/home");
     return {
-      sectionsData
+      page
     };
   }
 
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    return (
-      <BaseLayout {...this.props} title="Home">
-        {this.props.sectionsData.map(section => {
-          return <SectionCard {...section} key={section.id} />;
-        })}
-
-        <section>
-          <div className="container">
-            <Player
-              playlist={[
-                {
-                  id: 1,
-                  title: "Item 1 - Playlist",
-                  sources: [
-                    {
-                      src: "https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8",
-
-                      type: "application/x-mpegURL"
-                    }
-                  ]
-                },
-                {
-                  id: 2,
-                  title: "Item 2 - Playlist",
-                  sources: [
-                    {
-                      src:
-                        "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-                      type: "application/x-mpegURL"
-                    }
-                  ]
-                }
-              ]}
-            />
-          </div>
-        </section>
-      </BaseLayout>
-    );
+    return <BasePage page={this.props.page}></BasePage>;
   }
 }
 
