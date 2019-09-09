fe6a9adaa074c9577443b08526647561dc35a2a4
Marcos Gin <marcosgin291@gmail.com>
2019-09-09 15:43:33 -0300

    removing old components

--- components/Player/config.js
+++
@@ -1,6 +0,0 @@
-export const STATUS_LOADED = "loaded";
-export const STATUS_LOADING = "loading";
-export const STATUS_PLAYING = "playing";
-export const STATUS_WAITING = "waiting";
-export const STATUS_PAUSE = "paused";
-export const STATUS_ERROR = "error";
--- components/index.js
+++ components/index.js
@@ -1,10 +1,3 @@
 import dynamic from "next/dynamic";
 
-const Card = dynamic(() => import("./Card"));
-const CardList = dynamic(() => import("./CardList"));
-const Header = dynamic(() => import("./Header"));
-const Player = dynamic(() => import("./Player"));
-const SectionCard = dynamic(() => import("./SectionCard"));
-const SectionPosts = dynamic(() => import("./SectionPosts"));
-
-export default { Card, CardList, Header, Player, SectionCard, SectionPosts };
+export default {};
